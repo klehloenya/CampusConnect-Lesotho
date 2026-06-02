8d2ca98148802f10d3a3af421f71fd850beac540
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-06-02 16:03:58 +0200

    Implement updates for test

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Using relative paths to talk to our local Express proxy
 // This avoids CORS issues because the browser talks to the same origin
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://kkgq3q14-8000.inc1.devtunnels.ms/',
   headers: {
     'Content-Type': 'application/json',
   },
